054168f4d907d5dfa91550b6d02d680ffc6abc6a
iamben0 <benedict.ongjp@gmail.com>
2023-10-16 11:54:13 +0800

    added login component

---
+++ frontend/app/account/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+// After login, will appear as localhost:3000/account
+
+const Account = () => {
+	return <div>Welcome to your account!</div>;
+};
+
+export default Account;
