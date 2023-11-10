6f075bd08eca56c776cb99ba74a38b092ebd682e
iamben0 <benedict.ongjp@gmail.com>
2023-11-10 15:32:20 +0800

    added in test data

--- database/CreateTable.sql
+++ database/CreateTable.sql
@@ -12,16 +12,6 @@ CREATE TABLE user_profile
     active       BOOLEAN      NOT NULL DEFAULT TRUE
 );
 
-INSERT INTO user_profile (profile_type, job_title, active)
-VALUES ('admin' , 'senior system admin' , TRUE)  ,
-('admin'        , 'junior system admin' , TRUE)  ,
-('owner'        , 'owner'               , TRUE)  ,
-('manager'      , 'senior manager'      , TRUE)  ,
-('manager'      , 'junior manager'      , TRUE)  ,
-('staff'        , 'junior staff'        , TRUE)  ,
-('staff'        , 'senior staff'        , TRUE);
-
-
 CREATE TABLE user_account
 (
     id           SERIAL PRIMARY KEY,
@@ -34,20 +24,6 @@ CREATE TABLE user_account
     user_profile INTEGER REFERENCES user_profile (id)
 );
 
-INSERT INTO user_account (username, name, password, email, active, role, user_profile) VALUES
-       ('admin1'   , 'Robert Downey'      , 'password1' , 'ironman@gmail.com'     , TRUE , 'non-staff' , 1)  ,
-       ('admin2'   , 'Chris Evans'        , 'password2' , 'captamerica@gmail.com' , TRUE , 'non-staff' , 2)  ,
-       ('owner'    , 'Chris Hemsworth'    , 'password3' , 'thor@gmail.com'        , TRUE , 'non-staff' , 3)  ,
-       ('manager1' , 'Scarlett Johansson' , 'password4' , 'blackwidow@gmail.com'  , TRUE , 'non-staff' , 4)  ,
-       ('manager2' , 'Jeremy Renner'      , 'password5' , 'hawkeye@gmail.com'     , TRUE , 'non-staff' , 5)  ,
-       ('staff1'   , 'Mark Ruffalo'       , 'password6' , 'hulk@gmail.com'        , TRUE , 'chef'      , 6)  ,
-       ('staff2'   , 'Samuel L. Jackson'  , 'password7' , 'nightfury@gmail.com'   , TRUE , 'waiter'    , 7)  ,
-       ('staff3'   , 'Peter Parker'       , 'password8' , 'spiderman@gmail.com'   , TRUE , 'cashier'   , 7)  ,
-       ('staff4'   , 'person1'            , 'password6' , 'person1@gmail.com'     , TRUE , 'un-assign' , 6)  ,
-       ('staff5'   , 'person2'            , 'password7' , 'person2@gmail.com'     , TRUE , 'un-assign' , 7)  ,
-       ('staff6'   , 'person3'            , 'password8' , 'person3@gmail.com'     , TRUE , 'un-assign' , 7);
-
-
 CREATE TABLE work_slot
 (
     id           SERIAL PRIMARY KEY,
@@ -57,36 +33,6 @@ CREATE TABLE work_slot
     assigned     BOOLEAN      NOT NULL DEFAULT FALSE
 );
 
-INSERT INTO work_slot (shift, role, date, assigned) VALUES
-    ('morning'   , 'chef'    , '2023-12-01' , TRUE ) ,
-    ('morning'   , 'waiter'  , '2023-12-01' , TRUE ) ,
-    ('morning'   , 'cashier' , '2023-12-01' , TRUE ) ,
-    ('afternoon' , 'chef'    , '2023-12-01' , TRUE ) ,
-    ('afternoon' , 'waiter'  , '2023-12-01' , TRUE ) ,
-    ('afternoon' , 'cashier' , '2023-12-02' , TRUE ) ,
-    ('morning'   , 'chef'    , '2023-12-02' , TRUE ) ,
-    ('morning'   , 'waiter'  , '2023-12-02' , TRUE ) ,
-    ('morning'   , 'cashier' , '2023-12-02' , TRUE ) ,
-    ('afternoon' , 'chef'    , '2023-12-02' , TRUE ) ,
-    ('afternoon' , 'waiter'  , '2023-12-02' , TRUE ) ,
-    ('afternoon' , 'cashier' , '2023-12-02' , TRUE ) ,
-    ('morning'   , 'chef'    , '2023-12-03' , TRUE ) ,
-    ('morning'   , 'waiter'  , '2023-12-03' , TRUE ) ,
-    ('morning'   , 'cashier' , '2023-12-03' , TRUE ) ,
-    ('afternoon' , 'chef'    , '2023-12-03' , TRUE ) ,
-    ('afternoon' , 'waiter'  , '2023-12-03' , TRUE ) ,
-    ('morning'   , 'cashier' , '2023-12-03' , FALSE ) ,
-    ('afternoon' , 'chef'    , '2023-12-03' , FALSE ) ,
-    ('afternoon' , 'waiter'  , '2023-12-03' , FALSE ) ,
-    ('afternoon' , 'waiter'  , '2023-12-03' , FALSE ) ,
-    ('morning'   , 'cashier' , '2023-12-04' , FALSE ) ,
-    ('afternoon' , 'chef'    , '2023-12-04' , FALSE ) ,
-    ('afternoon' , 'waiter'  , '2023-12-04' , FALSE ) ,
-    ('afternoon' , 'waiter'  , '2023-12-04' , FALSE ) ,
-    ('morning'   , 'cashier' , '2023-12-05' , FALSE ) ,
-    ('afternoon' , 'chef'    , '2023-12-05' , FALSE ) ,
-    ('afternoon' , 'waiter'  , '2023-12-05' , FALSE ) ,
-    ('afternoon' , 'waiter'  , '2023-12-05' , FALSE ) ;
 
 CREATE TABLE bid
 (
@@ -96,21 +42,3 @@ CREATE TABLE bid
     status       VARCHAR(255) NOT NULL DEFAULT 'pending' CHECK (status IN ('pending', 'approved', 'rejected'))
 );
 
-INSERT INTO bid (work_slot_id, staff_id, status) VALUES
-(1  , 6 , 'approved' )   ,
-(2  , 7 , 'approved' )   ,
-(3  , 8 , 'approved' )   ,
-(4  , 6 , 'approved' )   ,
-(5  , 7 , 'approved' )   ,
-(6  , 8 , 'approved' )   ,
-(7  , 6 , 'approved' )   ,
-(8  , 7 , 'approved' )   ,
-(9  , 8 , 'approved' )   ,
-(10 , 6 , 'approved' )   ,
-(11 , 7 , 'approved' )   ,
-(12 , 8 , 'approved' )   ,
-(13 , 6 , 'approved' )   ,
-(14 , 7 , 'approved' )   ,
-(15 , 8 , 'approved' )   ,
-(16 , 6 , 'approved' )   ,
-(17 , 7 , 'approved' )   ;
